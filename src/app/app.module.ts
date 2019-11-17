b519abe241165712a9379900ace90efe5be134ea
Malti <maltidevnani26@gmail.com>
2019-11-17 10:54:08 -0500

    responsive design changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {AutosizeModule} from 'ngx-autosize';
-import {AutocompleteLibModule} from 'angular-ng-autocomplete';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import {Routes, RouterModule} from '@angular/router';
-import { CommonTagView } from './CreateNewForm/commonTagView/commonTagView.component'
-import { HomeComponent } from './home/home.component';
-import { HoldtagComponent } from './CreateNewForm/holdtag/holdtag.component';
-import { TpctagComponent } from './CreateNewForm/tpctag/tpctag.component';
-import { SpecialInstructionComponent } from './CreateNewForm/special-instruction/special-instruction.component';
-import { SidemenuComponent } from './CreateNewForm/sidemenu/sidemenu.component';
-import { QualityalertinComponent } from './CreateNewForm/qualityalertin/qualityalertin.component';
-import {PageNotFoundComponent}from './page-not-found/page-not-found.component';
-import {LoginComponent} from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AddPictureComponent } from './CreateNewForm/add-picture/add-picture.component';
-import {AuthGuardService} from './Service/auth-guard.service';
-import { GetTagDataComponent } from './CreateNewForm/get-tag-data/get-tag-data.component'
-import { DatePipe } from '@angular/common';
-import { UpdateTagDataComponent } from './CreateNewForm/update-tag-data/update-tag-data.component';
-import { ExternalIssueFormComponent } from './external-issue-form/external-issue-form.component';
-import { PartValueCalculatorComponent } from './external-issue-form/part-value-calculator/part-value-calculator.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AutosizeModule } from "ngx-autosize";
+import { AutocompleteLibModule } from "angular-ng-autocomplete";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { CommonTagView } from "./CreateNewForm/commonTagView/commonTagView.component";
+import { HomeComponent } from "./home/home.component";
+import { HoldtagComponent } from "./CreateNewForm/holdtag/holdtag.component";
+import { TpctagComponent } from "./CreateNewForm/tpctag/tpctag.component";
+import { SpecialInstructionComponent } from "./CreateNewForm/special-instruction/special-instruction.component";
+import { SidemenuComponent } from "./CreateNewForm/sidemenu/sidemenu.component";
+import { QualityalertinComponent } from "./CreateNewForm/qualityalertin/qualityalertin.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { LoginComponent } from "./login/login.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AddPictureComponent } from "./CreateNewForm/add-picture/add-picture.component";
+import { AuthGuardService } from "./Service/auth-guard.service";
+import { GetTagDataComponent } from "./CreateNewForm/get-tag-data/get-tag-data.component";
+import { DatePipe } from "@angular/common";
+import { UpdateTagDataComponent } from "./CreateNewForm/update-tag-data/update-tag-data.component";
+import { ExternalIssueFormComponent } from "./external-issue-form/external-issue-form.component";
+import { PartValueCalculatorComponent } from "./external-issue-form/part-value-calculator/part-value-calculator.component";
 
-const appRoutes : Routes = [
-  {path:'', component:LoginComponent},
-  {path:'login', component:LoginComponent},
-  {path: 'home', component:HomeComponent,  canActivate: [AuthGuardService]},
-  {path: 'newForm',component : CommonTagView , canActivate: [AuthGuardService],
-  children:[
-  {path:'',outlet:'sidemenu',component:SidemenuComponent},
-  {path:'', component : QualityalertinComponent},
-  {path:'qualityTag', component : QualityalertinComponent},
-  {path: 'holdTag',component : HoldtagComponent},
-  {path: 'tpcTag',component : TpctagComponent},
-  {path: 'specialInstruction',component : SpecialInstructionComponent},
-  {path: 'picture',component : AddPictureComponent}]},
-  {path: 'getTag',component : GetTagDataComponent , canActivate: [AuthGuardService]},
-  {path: 'updateTag/:id',component : UpdateTagDataComponent , canActivate: [AuthGuardService]},
-  {path: 'externalIssueForm', component:ExternalIssueFormComponent,  canActivate: [AuthGuardService]},
-  {path: 'partValueCalculator',component : PartValueCalculatorComponent , canActivate: [AuthGuardService]},
-  
-  { path: '**', component: PageNotFoundComponent },
- 
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "home", component: HomeComponent },
+  {
+    path: "newForm",
+    component: CommonTagView,
+    children: [
+      { path: "", outlet: "sidemenu", component: SidemenuComponent },
+      { path: "", component: QualityalertinComponent },
+      { path: "qualityTag", component: QualityalertinComponent },
+      { path: "holdTag", component: HoldtagComponent },
+      { path: "tpcTag", component: TpctagComponent },
+      { path: "specialInstruction", component: SpecialInstructionComponent },
+      { path: "picture", component: AddPictureComponent }
+    ]
+  },
+  { path: "getTag", component: GetTagDataComponent },
+  { path: "updateTag/:id", component: UpdateTagDataComponent },
+  { path: "externalIssueForm", component: ExternalIssueFormComponent },
+  { path: "partValueCalculator", component: PartValueCalculatorComponent },
+
+  { path: "**", component: PageNotFoundComponent }
 ];
-
 
 @NgModule({
   declarations: [
@@ -72,7 +74,7 @@ const appRoutes : Routes = [
     AutosizeModule,
     AutocompleteLibModule
   ],
-  providers: [AuthGuardService,DatePipe],
+  providers: [AuthGuardService, DatePipe],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
